10efd101f7ac959579edf850784c3ff55f83fb9d
Hindu <rajuhindur@gmail.com>
2024-12-07 11:38:00 +0530

    Add files via upload

---
+++ Breakcontinue.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Breakcontinue
+{
+    public static void main(String[] args)
+     {
+        System.out.println("break example");
+        for(int i=0;i<5;i++)
+        {
+            if(i==2)
+            break;
+            System.out.println(i);
+        }
+        System.out.println("Continue example");
+        for(int i=0;i<5;i++)
+        {
+            if(i==2)
+            continue;
+            System.out.println(i);      
+         }
+     }
+}
